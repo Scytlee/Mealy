13a6eaa3b756b0bde82fc959690718a5232ef75c
Scytlee <Scytlee@disroot.org>
2023-05-04 23:13:51 +0200

    Created Result objects

---
+++ src/Mealy.Domain/Shared/ResultT.cs
@@ -0,0 +1,15 @@
+﻿namespace Mealy.Domain.Shared;
+
+public class Result<TValue> : Result
+{
+  private readonly TValue? _value;
+
+  protected internal Result(TValue? value, bool isSuccess, Error error) : base(isSuccess, error)
+  {
+    _value = value;
+  }
+
+  public TValue? Value => IsSuccess ? _value : throw new InvalidOperationException();
+
+  public static implicit operator Result<TValue>(TValue? value) => new(value, true, Error.None);
+}
