4f3cad2c0d5700c9a12cdf7f15abfd2f91eddc57
Scytlee <Scytlee@disroot.org>
2023-05-04 22:56:50 +0200

    Created Error object

---
+++ src/Mealy.Domain/Shared/Error.cs
@@ -0,0 +1,50 @@
+﻿namespace Mealy.Domain.Shared;
+
+public sealed class Error : IEquatable<Error>
+{
+  public static readonly Error None = new(string.Empty, string.Empty);
+
+  public Error(string code, string message)
+  {
+    Code = code;
+    Message = message;
+  }
+
+  public string Code { get; }
+  public string Message { get; }
+
+  public static implicit operator string(Error error) => error.Code;
+
+  public static bool operator ==(Error? first, Error? second)
+  {
+    if (first is null && second is null)
+    {
+      return true;
+    }
+
+    if (first is null || second is null)
+    {
+      return false;
+    }
+
+    return first.Equals(second);
+  }
+
+  public static bool operator !=(Error? first, Error? second) => !(first == second);
+
+  public bool Equals(Error? other)
+  {
+    if (other is null)
+    {
+      return false;
+    }
+
+    return Code == other.Code && Message == other.Message;
+  }
+
+  public override bool Equals(object? obj) => obj is Error error && Equals(error);
+
+  public override int GetHashCode() => HashCode.Combine(Code, Message);
+
+  public override string ToString() => Code;
+}
