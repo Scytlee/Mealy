1f95b0c4db27dc6a76d6854b6590183d35410087
Scytlee <Scytlee@disroot.org>
2023-05-04 22:31:49 +0200

    Created Entity class

---
+++ src/Mealy.Domain/Primitives/Entity.cs
@@ -0,0 +1,61 @@
+﻿namespace Mealy.Domain.Primitives;
+
+public abstract class Entity : IEquatable<Entity>
+{
+  public long Id { get; private init; }
+  
+  protected Entity(long id)
+  {
+    Id = id;
+  }
+
+  public static bool operator ==(Entity? first, Entity? second)
+  {
+    return first is not null && second is not null && first.Equals(second);
+  }
+
+  public static bool operator !=(Entity? first, Entity? second)
+  {
+    return !(first == second);
+  }
+
+  public bool Equals(Entity? other)
+  {
+    if (other is null)
+    {
+      return false;
+    }
+
+    if (other.GetType() != GetType())
+    {
+      return false;
+    }
+
+    return Id == other.Id;
+  }
+
+  public override bool Equals(object? obj)
+  {
+    if (obj is null)
+    {
+      return false;
+    }
+
+    if (obj.GetType() != GetType())
+    {
+      return false;
+    }
+
+    if (obj is not Entity entity)
+    {
+      return false;
+    }
+
+    return Id == entity.Id;
+  }
+
+  public override int GetHashCode()
+  {
+    return Id.GetHashCode() * 37;
+  }
+}
