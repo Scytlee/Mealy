27755533ce67c63f7dfebe8d1a8ca341b8cdae57
Damian Jazienicki <damian.jazienicki@gmail.com>
2023-05-06 18:56:40 +0200

    Improved ValueObjects

--- src/Mealy.Domain/Primitives/ValueObject.cs
+++ src/Mealy.Domain/Primitives/ValueObject.cs
@@ -4,11 +4,15 @@ public abstract class ValueObject : IEquatable<ValueObject>
 {
   public abstract IEnumerable<object> GetAtomicValues();
 
+  private bool ValuesAreEqual(ValueObject other) => GetAtomicValues().SequenceEqual(other.GetAtomicValues());
+
   public bool Equals(ValueObject? other) => other is not null && ValuesAreEqual(other);
 
   public override bool Equals(object? obj) => obj is ValueObject other && ValuesAreEqual(other);
+  
+  public static bool operator ==(ValueObject? first, ValueObject? second) => first is not null && second is not null && first.Equals(second);
+
+  public static bool operator !=(ValueObject? first, ValueObject? second) => !(first == second);
 
   public override int GetHashCode() => GetAtomicValues().Aggregate(default(int), HashCode.Combine);
-
-  private bool ValuesAreEqual(ValueObject other) => GetAtomicValues().SequenceEqual(other.GetAtomicValues());
 }
---
+++ src/Mealy.Domain/Primitives/ValueObjectT.cs
@@ -0,0 +1,19 @@
+﻿namespace Mealy.Domain.Primitives;
+
+public abstract class ValueObject<T> : ValueObject
+  where T : notnull
+{
+  public T Value { get; }
+  
+  protected ValueObject(T value)
+  {
+    Value = value;
+  }
+
+  public static implicit operator T(ValueObject<T> valueObject) => valueObject.Value;
+
+  public override IEnumerable<object> GetAtomicValues()
+  {
+    yield return Value;
+  }
+}
