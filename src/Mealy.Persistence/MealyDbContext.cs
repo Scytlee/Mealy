648155946fb2dd8f0889820ed246b1e915c40d37
Damian Jazienicki <damian.jazienicki@gmail.com>
2023-05-07 20:05:31 +0200

    Configured DateOnly and TimeOnly value conversion

--- src/Mealy.Persistence/MealyDbContext.cs
+++ src/Mealy.Persistence/MealyDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Mealy.Persistence;
 
@@ -8,6 +9,41 @@ public sealed class MealyDbContext : DbContext
   public MealyDbContext(DbContextOptions options) : base(options) {}
 
   protected override void OnModelCreating(ModelBuilder modelBuilder) => modelBuilder.ApplyConfigurationsFromAssembly(AssemblyReference.Assembly);
+
+  protected override void ConfigureConventions(ModelConfigurationBuilder builder)
+  {
+    builder.Properties<DateOnly>()
+           .HaveConversion<DateOnlyConverter>()
+           .HaveColumnType("date");
+    
+    builder.Properties<TimeOnly>()
+           .HaveConversion<TimeOnlyConverter>()
+           .HaveColumnType("time");
+    
+    base.ConfigureConventions(builder);
+  }
+}
+
+// ReSharper disable once ClassNeverInstantiated.Global
+internal sealed class DateOnlyConverter : ValueConverter<DateOnly, DateTime>
+{
+  public DateOnlyConverter() : base(
+    dateOnly => dateOnly.ToDateTime(TimeOnly.MinValue), 
+    dateTime => DateOnly.FromDateTime(dateTime))
+  {
+    
+  }
+}
+
+// ReSharper disable once ClassNeverInstantiated.Global
+internal sealed class TimeOnlyConverter : ValueConverter<TimeOnly, TimeSpan>
+{
+  public TimeOnlyConverter() : base(
+    timeOnly => timeOnly.ToTimeSpan(), 
+    timeSpan => TimeOnly.FromTimeSpan(timeSpan))
+  {
+    
+  }
 }
 
 internal sealed class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<MealyDbContext>
