fd4fed361c9c6ad7289c0c16200d76e1d938c4b9
Damian Jazienicki <damian.jazienicki@gmail.com>
2023-05-07 15:16:00 +0200

    Created CQRS messaging abstractions

---
+++ src/Mealy.Application/Abstractions/Messaging/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using Mealy.Domain.Common.Validation;
+using MediatR;
+
+namespace Mealy.Application.Abstractions.Messaging;
+
+public interface ICommand : IRequest<Result>
+{
+  
+}
+
+public interface ICommand<TResponse> : IRequest<Result<TResponse>>
+{
+  
+}
---
+++ src/Mealy.Application/Abstractions/Messaging/ICommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using Mealy.Domain.Common.Validation;
+using MediatR;
+
+namespace Mealy.Application.Abstractions.Messaging;
+
+public interface ICommandHandler<TCommand> : IRequestHandler<TCommand, Result>
+  where TCommand : ICommand
+{
+  
+}
+
+public interface ICommandHandler<TCommand, TResponse> : IRequestHandler<TCommand, Result<TResponse>>
+   where TCommand : ICommand<TResponse>
+{
+  
+}
