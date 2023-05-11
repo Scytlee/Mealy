4964b8133d2668e8012f8d1c636f519e6d1189d0
Damian Jazienicki <damian.jazienicki@gmail.com>
2023-05-11 15:39:37 +0200

    Created console application for testing

---
+++ src/Mealy.ConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using Mealy.Application.Products.Queries.GetProducts;
+using Mealy.Application.Products.Repositories;
+using Mealy.Persistence;
+using Mealy.Persistence.Products.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Diagnostics;
+
+var host = Host.CreateDefaultBuilder(args)
+               .ConfigureServices((hostContext, services) =>
+               {
+                 services.AddMediatR(options =>
+                 {
+                   options.RegisterServicesFromAssembly(Mealy.Application.AssemblyReference.Assembly);
+                 });
+
+                 services.AddScoped<IProductRepository, ProductRepository>();
+
+                 services.AddDbContext<MealyDbContext>(options =>
+                 {
+                   options.UseSqlServer(hostContext.Configuration.GetConnectionString("Mealy"));
+                 });
+               }).Build();
+
+var mediator = host.Services.GetRequiredService<ISender>();
+
+var stopwatch = Stopwatch.StartNew();
+await mediator.Send(new GetProductsQuery());
+stopwatch.Stop();
+Console.WriteLine(stopwatch.ElapsedMilliseconds);
